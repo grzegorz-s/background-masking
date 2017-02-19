390cc3a1df68a775c46efedf1419a3f238625724
Grzegorz Sajnog <sajnog.grzegorz@gmail.com>
2017-02-19 14:02:35 +0100

    first case scanario done

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,7 +2,7 @@ function scrollDown() {
   $('.js-scroll-down').on('click', function() {
     $('html, body').animate({
         scrollTop: $('.js-section').offset().top
-    }, 2
+    },
     400);
   });
 }
