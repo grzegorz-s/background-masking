ea55414bb7bd97f278f5559a19a26b8df23aa3b5
Grzegorz Sajnog <sajnog.grzegorz@gmail.com>
2017-02-19 11:56:54 +0100

    banner, two-blocks, content section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
-console.log('\'Allo \'Allo!');
+function scrollDown() {
+  $('.js-scroll-down').on('click', function() {
+    $('html, body').animate({
+        scrollTop: $('.js-section').offset().top
+    }, 2
+    400);
+  });
+}
+
+scrollDown();
